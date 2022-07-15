aa6cef10008fcea07ec94a6a39b44946aefdee61
pluckannfeel <padayao.jm96@gmail.com>
2022-07-15 14:59:22 +0900

    added date and time helper

---
+++ helpers/datetime.py
@@ -0,0 +1,23 @@
+from datetime import datetime, date
+
+
+def get_date_time_now():
+    now = datetime.now()
+
+    return now.strftime("%d/%m/%Y %H:%M:%S")
+
+
+def get_date_now(type_format: str) -> str:
+    today = date.today()
+    
+    if(type_format == 'd1'):
+        # dd/mm/YY
+        return today.strftime("%d/%m/%Y")
+    elif(type_format == 'd2'):
+        # Textual month, day and year
+        return today.strftime("%B %d, %Y")
+    elif(type_format == 'd3'):
+        # mm/dd/y
+        return today.strftime("%m/%d/%y")
+    elif(type_format == 'd4'):
+        return today.strftime("%b-%d-%Y")
