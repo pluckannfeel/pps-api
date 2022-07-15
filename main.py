466b19e871de4ab9a69d4507498c3eadc88d046f
pluckannfeel <padayao.jm96@gmail.com>
2022-07-15 14:58:56 +0900

    added logging

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-#FastAPI
-from lib2to3.pgen2 import token
+import logging
+import random
+import string
+import time
+from datetime import datetime
+
+# FastAPI
+from tokenize import String
+from urllib.request import Request
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
@@ -7,8 +14,17 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 # api routers
 from routers.users import router as userRouter
-#database
+# database
 from db.init import initialize_db
+
+#helpers
+from helpers.datetime import get_date_time_now
+
+
+# setup loggers
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+# get root logger
+logger = logging.getLogger(__name__)
 
 
 app = FastAPI(title="PPS API", version="0.1.1",
@@ -16,10 +32,10 @@ app = FastAPI(title="PPS API", version="0.1.1",
 
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-#static file setup config
+# static file setup config
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-#ROUTERS
+# ROUTERS
 app.include_router(userRouter)
 
 origins = [
@@ -33,6 +49,29 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=['*'],
                    allow_headers=['*']
                    )
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    # writes to log.txt
+    file = open('test.log', 'a+')
+    
+    idem = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem} start request path={request.url.path}")
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = '{0:.2f}'.format(process_time)
+    logger.info(
+        f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}")
+    
+    
+    write_log = f'{get_date_time_now()} request path={request.url.path} rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code} '
+    file.write(write_log + '\n')
+    
+    return response
 
 
 @app.get('/')
